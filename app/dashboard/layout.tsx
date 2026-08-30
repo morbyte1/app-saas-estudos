b9c75973ca2a5e657fa65d903cd56ef923e9e7d6
morbyte1 <arthuralaebito@gmail.com>
2026-08-30 11:39:08 -0300

    Atualizacao de progresso

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -6,7 +6,8 @@ export default function DashboardLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className="flex h-screen bg-slate-50 overflow-hidden">
+    // Alterado h-screen para h-[100dvh]
+    <div className="flex h-[100dvh] bg-slate-50 overflow-hidden">
       <Sidebar />
       {/* Adicionado animate-page na tag main para animar todas as transições de rota internamente */}
       <main className="flex-1 overflow-y-auto pt-16 md:pt-0 animate-page">{children}</main>
